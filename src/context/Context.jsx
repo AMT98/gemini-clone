31d10a86638ae4a74e261820d6fc84c7a0a20370
AMT98 <aswinmalla12@gmail.com>
2024-11-29 14:36:43 -0700

    Added responsiveness to the page

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -15,13 +15,25 @@ const ContextProvider = (props) => {
     }, 75 * index);
   };
 
+  const newChat = () => {
+    setLoading(false)
+    setShowResult(false)
+  }
+
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
-    setRecentPrompt(input);
-    setPreviousPrompt((prev) => [...prev, input]);
-    const response = await run(input);
+    let response;
+    if (prompt !== undefined) {
+      response = await run(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPreviousPrompt((prev) => [...prev, input]);
+      setRecentPrompt(input);
+      response = await run(input);
+    }
+
     let responseArray = response.split("**");
     let newResponse = "";
     for (let i = 0; i < responseArray.length; i++) {
@@ -52,6 +64,7 @@ const ContextProvider = (props) => {
     resultData,
     input,
     setInput,
+    newChat
   };
 
   return (
