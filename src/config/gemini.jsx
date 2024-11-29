76f8e578703139607f18d5f2cbf3f5b8c05e9139
AMT98 <aswinmalla12@gmail.com>
2024-11-29 13:32:44 -0700

    Fixed error with typing animation

--- src/config/gemini.jsx
+++ src/config/gemini.jsx
@@ -26,7 +26,6 @@ async function run(prompt) {
   });
 
   const result = await chatSession.sendMessage(prompt);
-  console.log(result.response.text());
   const response = result.response;
   return response.text();
 }
