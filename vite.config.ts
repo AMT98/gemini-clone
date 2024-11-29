1463d31bcc4e5ced34b200880f1a6ddad191acd6
AMT98 <aswinmalla12@gmail.com>
2024-11-29 11:25:33 -0700

    Added gemini apiKey

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+if (typeof globalThis.structuredClone === 'undefined') {
+  globalThis.structuredClone = (obj: any): any => JSON.parse(JSON.stringify(obj));
+}
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
     hmr: true,
+    watch: {
+      usePolling: true,
+    },
   },
 })
