410217e244094d62d9dc8824e369e6d184b41e0e
AMT98 <aswinmalla12@gmail.com>
2024-12-11 19:44:38 -0700

    Changed main icon

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -18,7 +18,7 @@ const Main = () => {
     <div className="main">
       <div className="nav">
         <p>Gemini</p>
-        <img src={assets.user_icon} alt="" />
+        <img src={assets.gemini_icon} alt="" />
       </div>
       <div className="main-container">
         {!showResult ? (
